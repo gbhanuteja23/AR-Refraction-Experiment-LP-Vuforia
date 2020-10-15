ca40600dc2ca7e3606891368911e7ea49b68c536
gbhanuteja23 <g.bhanuteja23@gmail.com>
2020-10-15 09:19:10 +0530

    change_final_15-10-20

---
+++ Assets/Scripts/CarMovementAndroid.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovementAndroid : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    //AudioSource audioSource;
+
+    public float rcsThrust = 350f;
+
+    float rotateThisFrame;
+
+    public float forceval = 350f;
+
+    public Quaternion angle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();       
+
+        //audioSource = GetComponent<AudioSource>();       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+    public void ThrustUpward()
+    {
+        if (!audioSource.isPlaying)  
+        {
+            audioSource.Play();    
+        }
+
+        else
+        {
+            audioSource.Stop();   // To stop playing rocketSound
+        }
+
+        rigidBody.AddRelativeForce(new Vector3(0,0,1) * forceval);
+    }
+
+    public void ThrustDownward()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+
+        else
+        {
+            audioSource.Stop();   // To stop playing rocketSound
+        }
+
+        rigidBody.AddRelativeForce( new Vector3(0,0,-1) * forceval);
+    }
+
+    */
+
+    public void MoveLeft()
+    {
+        
+        rigidBody.AddRelativeForce(new Vector3(-1, 0, 0) * forceval);
+    }
+
+    public void MoveRight()
+    {
+
+        rigidBody.AddRelativeForce(new Vector3(1, 0, 0) * forceval);
+    }
+
+    public void MoveForward()
+    {
+
+        rigidBody.AddRelativeForce(new Vector3(0, 0, 1) * forceval);
+    }
+
+    public void Movebackward()
+    {
+
+        rigidBody.AddRelativeForce(new Vector3(0, 0, -1) * forceval);
+    }
+
+    public void RotateClockwise_x()
+    {
+        rotateThisFrame = rcsThrust * Time.deltaTime;
+
+        transform.Rotate(new Vector3(0, 1, 0) * rotateThisFrame);
+    }
+    
+
+    public void RotateAntiClockwise_x()
+    {
+        rotateThisFrame = rcsThrust * Time.deltaTime;
+
+        transform.Rotate(-new Vector3(0, 1, 0) * rotateThisFrame);
+    }
+
+    public void RotateClockwise_y()
+    {
+        rotateThisFrame = rcsThrust * Time.deltaTime;
+
+        transform.Rotate(new Vector3(0, 1, 0) * rotateThisFrame);
+    }
+
+
+    public void RotateAntiClockwise_y()
+    {
+        rotateThisFrame = rcsThrust * Time.deltaTime;
+
+        transform.Rotate(-new Vector3(0, 1, 0) * rotateThisFrame);
+    }
+
+    public void RotateClockwise_z()
+    {
+        rotateThisFrame = rcsThrust * Time.deltaTime;
+
+        transform.Rotate(new Vector3(1, 0, 0) * rotateThisFrame);
+    }
+
+
+    public void RotateAntiClockwise_z()
+    {
+        rotateThisFrame = rcsThrust * Time.deltaTime;
+
+        transform.Rotate(-new Vector3(1, 0, 0) * rotateThisFrame);
+    }
+
+    void ReduceSpeed()
+    {
+        rigidBody.AddRelativeForce(new Vector3(0, 0, -1) * forceval);
+
+        //rigidBody.velocity = new Vector3(0, 0, 0.5f);
+
+        Debug.Log("Speed Changed");
+    }
+
+    void ChangeAngle()
+    {
+        
+        angle.eulerAngles = new Vector3(0, -170, 0);
+
+        Debug.Log("Angle Changed");
+    }
+
+    public void ResetPosCar()
+    {
+        this.gameObject.transform.position = new Vector3(801.478f, 48.353f, 109.761f);
+
+        this.gameObject.transform.rotation = Quaternion.Euler(79.748f, -180.90f, -0.625f);
+
+        rigidBody.velocity = new Vector3(0,0,0);
+    }
+    
+    //Check for Collision with Trigger and Reduce the speed of Car and change angle in other Medium
+    private void OnTriggerEnter(Collider other)
+    {
+        ReduceSpeed();
+        ChangeAngle();
+    }
+}
---
+++ Assets/Scripts/CarMovementWindows.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovementWindows : MonoBehaviour
+{
+    public Quaternion angle;
+
+    public float forceval = 350f;
+
+    Rigidbody rigidBody;    // declaring variable rigidBody of type RigidBody data type. 
+
+    AudioSource carSound;   //declaring variable rocketSound of type AudioSource
+
+    [SerializeField] float moveCarForce = 100f;   //f states that 100 is a floating no.  rcs stands for rection control system
+                                               //variable to control speed of rocket
+
+    //Serializing private field lets you control it in Unity Inspector
+
+    [SerializeField] float mainAcceleartion = 100f;   //mainThrust variable to change the thrust of ship depending on it's mass
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();       //GetComponent is a functions which fetches RigidBodies here
+
+        //carSound = GetComponent<AudioSource>();   //Getcomponent fetches AudioSource to play
+
+        //rigidBody.freezeRotation = true;
+
+        //rigidBody.constraints = RigidbodyConstraints.FreezeRotationY;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Accelerate();          // Function which handles thrusting of rocket
+
+        
+
+    }
+    // Unity uses left handed system, this means pressing A turns anti-clockwise
+    //and pressing D, turns clockwise direction
+
+    //Vector3 is a struct which stores the coordinates of the bodies
+
+    public void Accelerate()       //Function which handles Acceleartion of Car
+    {
+        
+
+        
+        if (Input.GetKey(KeyCode.UpArrow))   //KeyCode is an enum where all the key values of keyboard are stored
+        {
+            rigidBody.AddForce(new Vector3(0,-1,0) * mainAcceleartion);          // Press Space for Thrusting the rocket upwards
+            
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidBody.AddForce(new Vector3(0, 1, 0) * mainAcceleartion);          // Press Space for Thrusting the rocket upwards
+            
+
+        }
+
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidBody.AddForce(new Vector3(-1, 0, 0) * mainAcceleartion);          // Press Space for Thrusting the rocket upwards
+          
+
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidBody.AddForce(new Vector3(1, 0, 0) * mainAcceleartion);          // Press Space for Thrusting the rocket upwards           
+
+        }
+
+        else if (Input.GetKey(KeyCode.RightControl))
+        {
+            rigidBody.AddForce(new Vector3(0, 1, 0) * mainAcceleartion);          // Press Space for Thrusting the rocket upwards
+            
+
+        }
+
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            rigidBody.AddForce(new Vector3(0, -1, 0) * mainAcceleartion);          // Press Space for Thrusting the rocket upwards
+            
+
+        }
+
+
+        else if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * mainAcceleartion*Time.deltaTime);     //To rotate in clockwise direction (+y axis)
+
+
+        }
+
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(-new Vector3(0, 1, 0) * mainAcceleartion * Time.deltaTime);     //To rotate in clockwise direction (+y axis)
+
+
+        }
+
+
+    }
+
+    /*
+
+    void ReduceSpeed()
+    {
+        rigidBody.AddRelativeForce(new Vector3(0, 0, -1) * forceval);
+
+        Debug.Log("Speed Changed");
+    }
+
+    void ChangeAngle()
+    {
+
+        angle.eulerAngles = new Vector3(0, -170, 0);
+
+        Debug.Log("Angle Changed");
+    }
+
+    public void ResetPosCar()
+    {
+        this.gameObject.transform.position = new Vector3(801.478f, 48.353f, 109.761f);
+    }
+
+    //Check for Collision with Trigger and Reduce the speed of Car and change angle in other Medium
+    private void OnTriggerEnter(Collider other)
+    {
+        ReduceSpeed();
+        ChangeAngle();
+    }
+
+    */
+}
