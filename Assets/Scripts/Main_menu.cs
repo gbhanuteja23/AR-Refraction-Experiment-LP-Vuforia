a408d2a8a10058fbce9555a77e7cc4f12d4dae58
gbhanuteja23 <g.bhanuteja23@gmail.com>
2020-10-23 14:09:36 +0530

    change_2

---
+++ Assets/Scripts/Main_menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_menu : MonoBehaviour
+{
+    public void StartAPP()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitAPP()
+    {
+        Application.Quit();  
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
